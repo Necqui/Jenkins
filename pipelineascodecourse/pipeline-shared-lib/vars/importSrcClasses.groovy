689ed331669b3fef8ffad95ccde54e21612d296f
Necqui Teja <Necqui@hotmail.com>
2021-09-07 12:48:03 -0700

    Delete pipelineascodecourse directory
    Deleted pipeline-shared-lib

--- pipelineascodecourse/pipeline-shared-lib/vars/importSrcClasses.groovy
+++
@@ -1,9 +0,0 @@
-import com.acceleratedskillup.*;
-
-def call(script) {
-    def myCar = new Car(script)
-	myCar.numberOfDoors = 5
-	myCar.brand = "123"
-	myCar.model = "abc"
-	myCar.printOutCar()
-}
